04992e7914e21264aa07252f2e64ff9067aef806
adhuliya <dhuliya@cse.iitb.ac.in>
2020-03-08 22:07:20 +0530

    semi-automated-update

--- c/delit.c
+++ c/delit.c
@@ -1,20 +1,5 @@
-
-#include <stdio.h>
+int x = 23;
 
 int main() {
-  // array of function pointers
-  int (*f)(int, char);
-  int (*arr[10])(int, char);
-  //int ((*f)[10](*)(int, char));
-//  int x = 10;
-//  switch(x) {
-//    case 11:
-//      printf("11\n");
-//      break;
-//    default:
-//      printf("default\n");
-//    case 10:
-//      printf("Hello\n");
-//  }
-//  return 0;
+  return x;
 }
